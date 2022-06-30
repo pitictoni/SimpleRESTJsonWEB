46192b52e163f0a8e840aaffe5e2805c0f0dccf0
antoniu.pitic <antoniu.pitic@gmail.com>
2022-06-30 12:34:16 +0300

    get all students

--- .idea/index.js
+++ .idea/index.js
@@ -9,9 +9,33 @@ async function getStudent(){
     emailTextArea.value = student.email;
 }
 
+async function getAllStudents(){
+    let response = await fetch('http://localhost:8080/getAllStudents');
+    let allStudents = await response.json();
+
+    populateTable(allStudents);
+}
+
+function populateTable(allStudents){
+    const studentsTable = document.getElementById('studentsTable');
+    for (const student of allStudents){
+        const row = document.createElement('tr');
+        const nameTd = document.createElement('td');
+        nameTd.innerHTML = student.name;
+        const emailTd = document.createElement('td');
+        emailTd.innerHTML = student.email;
+        row.appendChild(nameTd);
+        row.appendChild(emailTd);
+        studentsTable.appendChild(row);
+    }
+}
+
 function initPage(){
     const button = document.getElementById('getStudentButton')
     button.addEventListener("click", getStudent);
+    const allStudentsButton = document.getElementById('getAllStudentsButton')
+    allStudentsButton.addEventListener("click", getAllStudents);
+
 }
 
 
